d7f49ee42188c38ed21919ae6540baab825a999d
victuseze <106381679+victuseze@users.noreply.github.com>
2023-09-09 13:28:55 +0100

    Add files via upload

---
+++ stageone.js
@@ -0,0 +1,27 @@
+// const currentDayOfTheWeek = document.getElementById('currentDayOfTheWeek')
+// const currentUTCTime = document.getElementById('currentUTCTime')
+
+const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
+const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+
+const getDayOfTheWeek = () => {
+    const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const currentDate = new Date()
+    const day = currentDate.getDay()
+    const days = daysOfTheWeek[day]
+    console.log(days)
+    return days
+}
+
+const getUTCTiming = () => {
+    const currentDate = new Date()
+    const hour = currentDate.getUTCHours()
+    const minutes = currentDate.getUTCMinutes()
+    const seconds = currentDate.getUTCSeconds()
+    const UTCTiming = `${hour}: ${minutes}: ${seconds}` 
+    console.log(UTCTiming)
+    return UTCTiming
+}
+
+currentDayOfTheWeek.innerHTML = getDayOfTheWeek()
+currentUTCTime.innerHTML = getUTCTiming()
